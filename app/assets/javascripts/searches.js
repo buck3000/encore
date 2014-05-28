07bddd5d7f110f893ad4c25d960c8e5f0e6f7af5
Howard O'Leary <howardeoleary@gmail.com>
2014-05-28 14:46:38 -0400

    working on remove and uncheck song aspect of my feature

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -1,14 +1,14 @@
 var songsAndIds = {};
 var setListCheckOff = function(button, icon){
   $(button).css('background-color', 'green');
-  $(button).val('Add Song');
+  $(button).val('Added');
   $(button).removeClass('add-song').addClass('remove-song')
   $(icon).removeClass('fa-circle-o').addClass('fa-check-circle-o');
 }
 
 var removeListCheckOff = function(button, icon){
   $(button).css('background-color', 'red');
-  $(button).val('Added');
+  $(button).val('Add Song');
   $(button).removeClass('remove-song').addClass('add-song')
   $(icon).removeClass('fa-check-circle-o').addClass('fa-circle-o');
 }
@@ -26,16 +26,43 @@ $(document).ready(function() {
     console.log(button);
     songsAndIds[songId] = songTitle;
 
-    var songEl = ""
     var authenticity = $this.find('input[name=authenticity_token]').val();
 
-    songEl += "<p>"
-    songEl += songTitle + " - "
-    songEl += youtubeTitle
-    songEl += "</p>"
 
-    $('#added_songs').append(songEl);
-    
+    var setListedSong = $('ol.set-songs').find('li:contains('+ songTitle+ ')');
+    var icon = setListedSong.find('i');
+    setListCheckOff(button, icon);
+
+    $.post('/songs', { song: songTitle,
+                       song_id: songId,
+                       concert_id: concertId,
+                       artist_id: artistId,
+                       authenticity_token: authenticity})
+
+      .done(function(data) {
+        // var setListedSong = $('ol.set-songs').find('li:contains('+ songTitle+ ')');
+        // var icon = setListedSong.find('i');
+        $this.find('input[name=commit]').attr('disabled', 'disabled');
+        $('#flash').empty();
+        $('#flash').append('<div class="alert alert-success">' + data + '</div>')
+        // setListCheckOff(button, icon);
+
+      });
+  });
+
+  $('.remove-song').on("click", function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    console.log($this);
+
+    var youtubeTitle = $this.find('#yt-title').text();
+    var songId = $this.find('#song_id').val();
+    var songTitle = $this.find('select').val();
+    var button = $this.find('input#add-song');
+    console.log(button);
+    songsAndIds[songId] = songTitle;
+
+    var authenticity = $this.find('input[name=authenticity_token]').val();
 
     $.post('/songs', { song: songTitle,
                        song_id: songId,
